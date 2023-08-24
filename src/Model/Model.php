f5020ef87444ed852abea892593d30d1b15e9c89
Hrvoje Lesar <hrvoje.lesar1@hotmail.com>
2023-08-24 10:50:55 +0200

    Model find i toArray metode.

--- src/Model/Model.php
+++ src/Model/Model.php
@@ -62,4 +62,45 @@ abstract class Model
             $connection->update(static::getTableName(), $this->getModelData(), [[static::$primaryKeyColumn, "=", $id]]);
         }
     }
+
+    /**
+     * @return static|null
+     * @throws PDOException
+     */
+    public static function find(int $primaryKey): static|null
+    {
+        $item = Connection::getInstance()->select(sprintf("SELECT * FROM %s WHERE %s = %s", static::getTableName(), static::$primaryKeyColumn, $primaryKey))->fetchAssoc();
+        if ($item === false) {
+            return null;
+        }
+        $model = new static();
+        $model->setColumns($item);
+        return $model;
+    }
+
+    /**
+     * @param array $data
+     */
+    protected function setColumns(array $data): void
+    {
+        $columns = static::getTableColumns();
+        $this->{static::$primaryKeyColumn} = $data[static::$primaryKeyColumn];
+        foreach($columns as &$column) {
+            $this->$column = $data[$column];
+        }
+    }
+
+    public function toArray(): array
+    {
+        $id = $this->{static::$primaryKeyColumn};
+        $modelArray = isset($id) ? [static::$primaryKeyColumn => $id] : [];
+        $columns = static::getTableColumns();
+        foreach($columns as &$column) {
+            if (is_null($this->$column)) {
+                continue;
+            }
+            $modelArray[$column] = $this->$column;
+        }
+        return $modelArray;
+    }
 }
